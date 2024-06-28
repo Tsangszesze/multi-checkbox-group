b69ae351369d20c43a56c3cbd00056159680a347
TSANG Sze Sze, CC <tsangszesze@icloud.com>
2024-06-28 22:12:57 +0800

    feat: add input to control number of columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import './styles/App.css';
+import React, { useState } from "react";
+import "./styles/App.css";
+import { MultiCheckbox } from "./components/MultiCheckbox";
 
 function App() {
+  const [columns, setColumns] = useState<number>(2);
+  const OPTIONS = "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .map((val) => ({ label: val.repeat(3), value: val }));
+
   return (
     <div id="App">
+      <MultiCheckbox
+        options={OPTIONS}
+        onChange={(values) => console.log(values)}
+        columns={columns}
+      />
+      <input
+        type="number"
+        min={1}
+        max={99}
+        onChange={(e) => setColumns(Number(e.target.value))}
+      ></input>
     </div>
   );
 }
