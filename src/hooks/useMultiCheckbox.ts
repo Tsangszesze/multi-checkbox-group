4aa253467b32afb2ac368bcb82a472e168fdf4f2
TSANG Sze Sze, CC <tsangszesze@icloud.com>
2024-06-28 22:08:43 +0800

    feat: useMultiCheckbox manage selected values

---
+++ src/hooks/useMultiCheckbox.ts
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+interface useMultiCheckboxProps {
+  onChange: (values: string[]) => void;
+  defaultValues?: string[];
+}
+
+export const useMultiCheckbox = ({
+  onChange,
+  defaultValues,
+}: useMultiCheckboxProps) => {
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
+
+  const toggleCheckbox = (value: string) => {
+    const index = selectedValues.indexOf(value);
+    const temp = [...selectedValues];
+    index > -1 ? temp.splice(index, 1) : temp.push(value);
+    setSelectedValues(temp);
+    onChange(temp);
+  };
+
+  return { selectedValues, toggleCheckbox };
+};
