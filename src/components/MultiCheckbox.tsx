c61b547b1d941aa2fc70f46e773286b74a5c8349
TSANG Sze Sze, CC <tsangszesze@icloud.com>
2024-06-28 22:09:34 +0800

    feat: MultiCheckbox component mapping checkboxes

--- src/components/MultiCheckbox.tsx
+++ src/components/MultiCheckbox.tsx
@@ -1,3 +1,32 @@
-export const MultiCheckbox = () => {
-  return <div></div>;
+import { useMultiCheckbox } from "../hooks/useMultiCheckbox";
+import { CheckboxOption } from "../types/Checkbox";
+import { Checkbox } from "./Checkbox";
+
+interface MultiCheckboxProps {
+  options: CheckboxOption[];
+  defaultValues?: string[];
+  onChange: (values: string[]) => void;
+  columns: number;
+}
+
+export const MultiCheckbox = (props: MultiCheckboxProps) => {
+  const { options, defaultValues, onChange, columns } = props;
+
+  const { selectedValues, toggleCheckbox } = useMultiCheckbox({
+    onChange,
+    defaultValues,
+  });
+
+  return (
+    <div id="multi-checkbox" style={{gridTemplateColumns: `repeat(${columns}, 1fr)`}}>
+      {options.map((option, i) => (
+        <Checkbox
+          key={i}
+          label={option.label}
+          onCheck={() => toggleCheckbox(option.value)}
+          isChecked={selectedValues.includes(option.value)}
+        />
+      ))}
+    </div>
+  );
 };
