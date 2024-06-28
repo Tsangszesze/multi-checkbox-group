81578b93ce470f6009dbe420aa2061371f7a243d
TSANG Sze Sze, CC <tsangszesze@icloud.com>
2024-06-28 22:06:32 +0800

    feat: checkbox component

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,3 +1,15 @@
-export const Checkbox = () => {
-    return <div></div>;
-  };
+interface CheckboxProps {
+  isChecked: boolean;
+  onCheck: () => void;
+  label: string;
+}
+
+export const Checkbox = (props: CheckboxProps) => {
+  const { isChecked, onCheck, label } = props;
+  return (
+    <label>
+      <input type="checkbox" checked={isChecked} onChange={onCheck} />
+      <span>{label}</span>
+    </label>
+  );
+};
